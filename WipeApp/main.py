71ba6af3f2f1f2b7e25b13e90cdad683213ced1a
NIKOLAY_FILATOV <54846705+Russian-Hack@users.noreply.github.com>
2024-07-04 15:57:55 -0400

    Interface de base créée

--- WipeApp/main.py
+++ WipeApp/main.py
@@ -7,16 +7,14 @@ import tkinter
 from cProfile import label
 
 
-
+#Code base interface.
 root = tkinter.Tk()
-
+root.geometry("1920x1080")
 wipe = tkinter.Button(root, text="Wipe", command=print("wipe"))
-
+root.configure(bg="#35454F")
+wipe.pack()
 mac=tkinter.Button(root,text="Addresse Mac",command=print("IP"))
 mac.pack()
-
 quit= tkinter.Button(master=root, text="Exit", command=root.quit)
-
 quit.pack()
-
 root.mainloop()
