4cc84f778f7f99eb09e7ddc0945317434037bc4e
NIKOLAY_FILATOV <54846705+Russian-Hack@users.noreply.github.com>
2024-07-04 15:50:04 -0400

    V0.0001

--- WipeApp/main.py
+++ WipeApp/main.py
@@ -1,16 +1,22 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import curses
+
+import tkinter
+from cProfile import label
+
+
+
+root = tkinter.Tk()
+
+wipe = tkinter.Button(root, text="Wipe", command=print("wipe"))
+
+mac=tkinter.Button(root,text="Addresse Mac",command=print("IP"))
+mac.pack()
+
+quit= tkinter.Button(master=root, text="Exit", command=root.quit)
+
+quit.pack()
+
+root.mainloop()
