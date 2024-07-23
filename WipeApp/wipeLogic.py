609ca72ffcd98e8834d56120625a86bab734a434
NIKOLAY_FILATOV <54846705+Russian-Hack@users.noreply.github.com>
2024-07-23 09:17:10 -0400

    Update wipeLogic.py

--- WipeApp/wipeLogic.py
+++ WipeApp/wipeLogic.py
@@ -21,17 +21,48 @@ class WipeLogic:
 
     def _wipe_thread(self):
         try:
-
-            # Attempt to unmount the device
-            subprocess.run(['sudo', 'umount', self.selected_drive], check=True)
+            if self.is_mounted(self.selected_drive):
+                subprocess.run(['sudo', 'umount', self.selected_drive], check=True)
 
             # Perform wiping operations
-            subprocess.run(['sudo', 'mkfs.ext4', '-F', self.selected_drive], check=True, input=b'YES\n')
-            subprocess.run(
-                ['sudo', 'cryptsetup', 'luksFormat', '--type', 'luks2', '--verify-passphrase', self.selected_drive],
-                check=True)
-            subprocess.run(['sudo', 'cryptsetup', 'luksClose', self.selected_drive], check=True)
-            subprocess.run(['sudo', 'mkfs.ext4', '-F', self.selected_drive], check=True, input=b'YES\n')
+            mkfs_command = ['sudo', 'mkfs.ext4', '-F', self.selected_drive]
+
+            # Open subprocess with stdin to provide 'yes' automatically
+            with subprocess.Popen(mkfs_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE) as proc:
+                try:
+                    outs, errs = proc.communicate(input=b'yes\n', timeout=15)
+                except subprocess.TimeoutExpired:
+                    proc.kill()
+                    outs, errs = proc.communicate()
+
+            # Provide passphrase securely using a temporary file
+            passphrase = b'MySecurePassphrase'
+            passphrase_file = '/tmp/passphrase_file'
+            with open(passphrase_file, 'wb') as f:
+                f.write(passphrase)
+
+            cryptsetup_command = ['sudo', 'cryptsetup', 'luksFormat', '--type', 'luks2', '--batch-mode', '--key-file',
+                                  passphrase_file, self.selected_drive]
+
+            subprocess.run(cryptsetup_command, check=True)
+
+            # Close the LUKS volume
+            subprocess.run(['sudo', 'cryptsetup', 'luksClose', '--batch-mode', 'ENCRYPTED'], check=True)
+
+            # Format again to ensure data is securely wiped
+            subprocess.run(['sudo', 'mkfs.ext4', '-F', self.selected_drive], check=True)
+            with subprocess.Popen(mkfs_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE) as proc:
+                try:
+                    outs, errs = proc.communicate(input=b'yes\n', timeout=15)
+                except subprocess.TimeoutExpired:
+                    proc.kill()
+                    outs, errs = proc.communicate()
+
+            # Mount the drive back (if needed)
+            mount_point = '/mnt/myluks'
+            subprocess.run(['sudo', 'mount', self.selected_drive, mount_point], check=True)
 
             # Show completion message
             messagebox.showinfo("Wipe Complete",
